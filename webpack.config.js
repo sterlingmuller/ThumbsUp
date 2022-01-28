8ba0cc5b6e22537423095a9c00a2be0220fba2e6
Mitchell <mitchellwgardner@gmail.com>
2022-01-27 20:04:42 -0700

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "client", "src");
+var DIST_DIR = path.join(__dirname, "client", "dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+};
