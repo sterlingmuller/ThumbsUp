53e169cb8128757c3c055e2f3bb181770b0f5c09
Prith <prith98@gmail.com>
2022-01-28 19:12:36 -0800

    init creation and database creation

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,1 +1,57 @@
-DROP DATABASE IF EXISTS blueocean
+DROP DATABASE IF EXISTS blueocean;
+CREATE DATABASE blueocean;
+\c blueocean;
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR (50) NOT NULL,
+  password VARCHAR (50) NOT NULL,
+  profile_picture VARCHAR (500)
+);
+
+CREATE TABLE driver_trips (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  start_address VARCHAR(500) NOT NULL,
+  end_address VARCHAR(500) NOT NULL,
+  start_time TIMESTAMP NOT NULL,
+  completed BOOLEAN DEFAULT false,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+/* finished up to here */
+
+CREATE TABLE rider_trips (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  id_driver_trips INT,
+  pending BOOLEAN DEFAULT true,
+  completed_driver_trips BOOLEAN,
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (id_driver_trips) REFERENCES driver_trips (id)
+  -- FOREIGN KEY (completed_driver_trips) REFERENCES driver_trips (completed)
+);
+
+CREATE TABLE completed_trips (
+  id SERIAL PRIMARY KEY,
+  id_driver_trips INT,
+  user_id INT,
+  rating INT,
+  FOREIGN KEY (id_driver_trips) REFERENCES driver_trips(id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE messages (
+  message_id SERIAL PRIMARY KEY,
+  id_driver_trips INT,
+  message_sender INT,
+  message_recepient INT,
+  message_body VARCHAR(500),
+  message_time TIMESTAMP,
+  FOREIGN KEY (id_driver_trips) REFERENCES driver_trips(id),
+  FOREIGN KEY (message_sender) REFERENCES users(user_id),
+  FOREIGN KEY (message_recepient) REFERENCES users(user_id)
+);
+
+
+
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,18 @@
-return ('hello world')
+import React, { useState, useEffect, useContext} from 'react';
+import axios from 'axios';
+import { MainContext } from '../contexts/MainContext.js';
+
+
+const App = () => {
+
+
+
+  return (
+    <div>
+      Hello Blue Ocean!
+    </div>
+  );
+
+}
+
+export default App;
