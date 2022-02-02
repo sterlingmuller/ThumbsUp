35f2af7879828be960affa23b9132beaef6de7dc
Ezra Pullido <ezra.pullido@gmail.com>
2022-02-01 21:13:12 -0500

    Added start time selector

--- client/src/components/DriverSelection/DiverOptions/driverAddTripMap.jsx
+++ client/src/components/DriverSelection/DiverOptions/driverAddTripMap.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef, useContext} from 'react'
-import { GoogleMap, useJsApiLoader, DirectionsService, DirectionsRenderer, StandaloneSearchBox} from '@react-google-maps/api';
-import {MainContext} from '../../../contexts/MainContext.js'
+import React, { useState, useEffect, useRef, useContext } from 'react'
+import { GoogleMap, useJsApiLoader, DirectionsService, DirectionsRenderer, StandaloneSearchBox } from '@react-google-maps/api';
+import { MainContext } from '../../../contexts/MainContext.js'
 import axios from 'axios';
 
 const librariesArray = ['places'];
@@ -19,20 +19,16 @@ function DriverTripMap(props) {
   let [newTrip, setNewTrip] = useState({});
   let [startPoint, setStartPoint] = useState('');
   let [endPoint, setEndPoint] = useState('');
-  let {currentUser} = useContext(MainContext);
+  let { currentUser } = useContext(MainContext);
   let [directionsRequest, setRequest] = useState({
     origin: 'New York, NY, USA',
     destination: 'New York, NY, USA',
-    travelMode: 'DRIVING',
-    drivingOptions: {
-      departureTime: new Date(Date.now()),
-      trafficModel: 'optimistic'
-    }
+    travelMode: 'DRIVING'
   });
 
   useEffect(() => {
     console.log('Use Effect Triggered')
-  },[directionsRequest])
+  }, [directionsRequest])
 
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: '',
@@ -48,7 +44,7 @@ function DriverTripMap(props) {
 
   const renderDirections = () => {
     console.log('Get Directions: ')
-    setRequest({...directionsRequest, origin: startPoint[0].formatted_address, destination: endPoint[0].formatted_address});
+    setRequest({ ...directionsRequest, origin: startPoint[0].formatted_address, destination: endPoint[0].formatted_address });
     setDirections(undefined);
   }
 
@@ -67,79 +63,84 @@ function DriverTripMap(props) {
       start_time: directionsResult.request.drivingOptions.departureTime,
       user_id: currentUser.userId
     })
-    .then((response) => {
-      console.log('AddTrip: ', response);
-    })
-    .catch((error) => {
-      console.log('AddTrip Error: ', error);
-    });
+      .then((response) => {
+        console.log('AddTrip: ', response);
+      })
+      .catch((error) => {
+        console.log('AddTrip Error: ', error);
+      });
   }
 
   return (
-    !isLoaded? <div>Loading</div>:
-    <div>
-      {console.log('Loading load script', startPoint, endPoint, directionsRequest)}
-      <GoogleMap
-        id='map'
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        {!directionsResult?<DirectionsService callback={directionsCallback} options={directionsRequest}/>:null}
-        {!directionsResult? null:<DirectionsRenderer directions={directionsResult}  onLoad={directionsRenderer => {
-                    console.log('DirectionsRenderer onLoad directionsRenderer: ', directionsResult)}}/>}
-        <></>
-        <StandaloneSearchBox
-          onLoad={onStartLoad}
-          onPlacesChanged={onStartChanged}
+    !isLoaded ? <div>Loading</div> :
+      <div>
+        {console.log('Loading load script', startPoint, endPoint, directionsRequest)}
+        <GoogleMap
+          id='map'
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
         >
-        <input
-        type="text"
-        placeholder="Start Point"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-          position: "absolute",
-          bottom: "9%"
-        }}
-        />
-      </StandaloneSearchBox>
-      <StandaloneSearchBox
-          onLoad={onEndLoad}
-          onPlacesChanged={onEndChanged}
-        >
-          <input
-        type="text"
-        placeholder="End Point"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-          position: "absolute",
-          bottom: '0'
-        }}
-        />
-        </StandaloneSearchBox>
-      </GoogleMap>
-      <button onClick={renderDirections}>Get Directions</button>
-      <button onClick={null}>Add Trip</button>
-    </div>
+          { /* Child components, such as markers, info windows, etc. */}
+          {!directionsResult ? <DirectionsService callback={directionsCallback} options={directionsRequest} /> : null}
+          {!directionsResult ? null : <DirectionsRenderer directions={directionsResult} onLoad={directionsRenderer => {
+            console.log('DirectionsRenderer onLoad directionsRenderer: ', directionsResult)
+          }} />}
+          <></>
+          <StandaloneSearchBox
+            onLoad={onStartLoad}
+            onPlacesChanged={onStartChanged}
+          >
+            <input
+              type="text"
+              placeholder="Start Point"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                bottom: "9%"
+              }}
+            />
+          </StandaloneSearchBox>
+          <StandaloneSearchBox
+            onLoad={onEndLoad}
+            onPlacesChanged={onEndChanged}
+          >
+            <input
+              type="text"
+              placeholder="End Point"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                bottom: '0'
+              }}
+            />
+          </StandaloneSearchBox>
+        </GoogleMap>
+        <button onClick={renderDirections}>Get Directions</button>
+        <button onClick={null}>Add Trip</button>
+        <label htmlFor="start-time">Departure Date and Time:</label>
+        <input type="datetime-local" id="start-time"
+          name="start-time" value="2022-02-01T00:00"
+          min="2022-02-01T00:00" max="2022-06-14T00:00" />
+      </div>
   )
 }
 
