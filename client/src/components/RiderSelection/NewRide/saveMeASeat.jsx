cc74d9765dd72284d902723efefb2349877676ae
mathew dziedzic <mdzie195@gmail.com>
2022-02-04 10:31:00 -0800

    last for real?/

--- client/src/components/RiderSelection/NewRide/saveMeASeat.jsx
+++ client/src/components/RiderSelection/NewRide/saveMeASeat.jsx
@@ -4,11 +4,8 @@ import axios from 'axios';
 import { Link, useNavigate } from "react-router-dom";
 import TripMap from '../../DriverSelection/DiverOptions/tripMap.jsx'
 import {Button} from 'react-bootstrap';
-<<<<<<< HEAD
 import { IoMdArrowRoundBack } from "react-icons/io";
 
-=======
->>>>>>> main
 export const SaveMeASeat = () => {
   const { selectedTrip, currentUser, currentPage, setCurrentPage, setUserId } = useContext(MainContext);
   const navigate = useNavigate();
@@ -27,7 +24,6 @@ export const SaveMeASeat = () => {
             navigate('/riderSearch');
         }}/>
       {!trip?<div>loading</div>:<div><TripMap trip={trip}/></div>}
-      <div className = 'save-me-a-seat-container'>
       <span className = 'SaveButton'> Ride look good? Tell the driver to </span>
       <Button variant='primary' onClick = {() => {
               axios.get(`/trips/driver?trip_id=${selectedTrip}`)
@@ -43,7 +39,6 @@ export const SaveMeASeat = () => {
                   }).then(() => navigate('/riderPortal'))
                 })
       }}> Save me a seat</Button>
-      </div>
     </div>
   );
 }
