e8a5bfe807df8d3f5e123a094ff480a4a2ab3bbd
Prith <prith98@gmail.com>
2022-02-05 14:08:11 -0800

    Created engine spec in package and modified database index

--- backend/database/index.js
+++ backend/database/index.js
@@ -2,12 +2,31 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 
-const pool = new Pool ({
-  user: process.env.USER,
+// const localConnection = new Pool ({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+// });
+
+const localConnection = {
   host: process.env.HOST,
-  database: process.env.DATABASE,
+  user: process.env.USER,
   password: process.env.PASSWORD,
-});
+  database: process.env.DATABASE
+};
+
+const herokuConnection = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+};
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+  ? herokuConnection
+  : localConnection
+);
+
 
 //repo stuff here for heroku
 pool.connect(err => {
